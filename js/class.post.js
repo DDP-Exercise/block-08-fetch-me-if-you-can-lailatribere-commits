f30bc2acdaf1a0473a445f8caa2b372e0da9d17c
lailatribere-commits <lailatribere@gmail.com>
2026-06-24 15:55:54 +0200

    Last HOMEWORK
    upsies

--- js/class.post.js
+++ js/class.post.js
@@ -17,3 +17,31 @@
  *  When printing a post, don't forget to make a button that
  *  loads the comments for the post. Once they are loaded, print them.
  *  *******************************************************/
+
+
+export default class Post {
+    #id;
+    #title;
+    #body;
+
+    constructor(id, title, body) {
+        this.#id    = id;
+        this.#title = title;
+        this.#body  = body;
+        this.comments = [];
+    }
+
+    get id()    { return this.#id;    }
+    get title() { return this.#title; }
+    get body()  { return this.#body;  }
+
+    print() {
+        return `
+        <div data-post-id="${this.#id}">
+            <h4>${this.#title}</h4>
+            <p>${this.#body}</p>
+            <button data-load-comments="${this.#id}">Load Comments</button>
+            <div data-comments-of="${this.#id}"></div>
+        </div>`;
+    }
+}
